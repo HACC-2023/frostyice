ffc7b5a46037f69b2d390f66d7e1723b76db3776
Giorgio <tran.giorgio@gmail.com>
2023-10-27 00:28:01 -1000

    Add Map Marker example that gets coords on drag

--- my-app/src/pages/experimental/click-map/index.jsx
+++ my-app/src/pages/experimental/click-map/index.jsx
@@ -1,9 +1,63 @@
+import Map, { Marker, NavigationControl } from "react-map-gl";
+import ControlPanel from "./ControlPanel";
+import Pin from "./Pin";
+import { useState, useCallback } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { INITIAL_VIEW_STATE } from "@/lib/mapconfig";
+
 const ClickableMap = () => {
+  const [marker, setMarker] = useState({
+    longitude: INITIAL_VIEW_STATE.longitude,
+    latitude: INITIAL_VIEW_STATE.latitude,
+    zoom: 5.6,
+  });
+  const [events, logEvents] = useState({});
+
+  const onMarkerDragStart = useCallback((event) => {
+    logEvents((_events) => ({ ..._events, onDragStart: event.lngLat }));
+  }, []);
+
+  const onMarkerDrag = useCallback((event) => {
+    logEvents((_events) => ({ ..._events, onDrag: event.lngLat }));
+
+    setMarker({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat,
+    });
+  }, []);
+
+  const onMarkerDragEnd = useCallback((event) => {
+    logEvents((_events) => ({ ..._events, onDragEnd: event.lngLat }));
+  }, []);
+
+  console.log("marker", marker);
   return (
-    <div>
-      map
+    <div className="min-h-screen">
+      <Map
+        initialViewState={INITIAL_VIEW_STATE}
+        reuseMaps
+        controller={true}
+        style={{ width: "600px", height: "400px" }}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+      >
+        <Marker
+          longitude={marker.longitude}
+          latitude={marker.latitude}
+          anchor="bottom"
+          draggable
+          onDragStart={onMarkerDragStart}
+          onDrag={onMarkerDrag}
+          onDragEnd={onMarkerDragEnd}
+        >
+          <Pin size={20} />
+        </Marker>
+
+        <NavigationControl />
+      </Map>
+      <ControlPanel events={events} />
     </div>
-  )
-}
+  );
+};
 
 export default ClickableMap;
