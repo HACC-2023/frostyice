8eb73c772b1a08b65c03182b95f006c82e829dc9
Giorgio <tran.giorgio@gmail.com>
2023-10-26 23:47:18 -1000

    Create an experimental client route

---
+++ my-app/src/pages/experimental/click-map/index.jsx
@@ -0,0 +1,9 @@
+const ClickableMap = () => {
+  return (
+    <div>
+      map
+    </div>
+  )
+}
+
+export default ClickableMap;
--- my-app/src/pages/experimental/index.jsx
+++ my-app/src/pages/experimental/index.jsx
@@ -1,83 +1,12 @@
-// import Map from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import { useState, useEffect } from "react";
+const { default: Link } = require("next/link");
 
-// const MapboxPage = () => {
-//   const [details, setDetails] = useState([]);
-//   const [coordinates, setCoordinates] = useState([]);
-
-//   useEffect(() => {
-//     const getData = async () => {
-//       const response = await fetch(
-//         "https://api-shipx-pl.easypack24.net/v1/points?per_page=28000"
-//       );
-
-//       const data = await response.json();
-//       setDetails(data.items);
-
-//       // Create an array of geo coordinates pairs
-//       const coords = data.items.map((item) => [
-//         item.location.longitude,
-//         item.location.latitude,
-//       ]);
-//       setCoordinates(coords);
-//     };
-//     getData();
-//   }, []);
-//   console.log(coordinates);
-//   return (
-//     <Map
-//       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-//       initialViewState={{
-//         longitude: -122.4,
-//         latitude: 37.8,
-//         zoom: 14,
-//       }}
-//       style={{ width: 600, height: 400 }}
-//       mapStyle="mapbox://styles/mapbox/streets-v9"
-//     />
-//   );
-// };
-
-// export default MapboxPage;
-
-import React, { useState, useEffect } from "react";
-import dynamic from "next/dynamic";
-
-const LocationAggregatorMap = dynamic(
-  () => import("@/components/map/LocationAggregatorMap"),
-  { ssr: false }
-);
-
-const HomePage = () => {
-  const [details, setDetails] = useState([]);
-  const [coordinates, setCoordinates] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(
-        "https://api-shipx-pl.easypack24.net/v1/points?per_page=28000"
-      );
-
-      const data = await response.json();
-      setDetails(data.items);
-
-      // Create an array of geo coordinates pairs
-      const coords = data.items.map((item) => [
-        item.location.longitude,
-        item.location.latitude,
-      ]);
-      setCoordinates(coords);
-    };
-    getData();
-  }, []);
-  console.log(coordinates);
-
+const ExperimentalPage = () => {
   return (
-    <div className="relative min-h-screen">
-      <LocationAggregatorMap data={coordinates} />
+    <div className="p-5 flex flex-col gap-3">
+      <Link className="underline text-blue-600" href="/experimental/heatmap">Heatmap Test</Link>
+      <Link className="underline text-blue-600" href="/experimental/click-map">Clicking on Map to get Location Test</Link>
     </div>
   );
 };
 
-export default HomePage;
+export default ExperimentalPage;
