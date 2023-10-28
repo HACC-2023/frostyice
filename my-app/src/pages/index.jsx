615b0190268625b32eb48a4c6adf4eae7d28a9ca
Shun <saitoshin2145@gmail.com>
2023-10-28 14:49:55 +0900

    issue-07: added the mobile navbar page

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,12 +1,13 @@
-
+import MobileNavbar from '@/components/navbar/MobileNavbar';
 const Home = () => {
   return (
-    <div className="min-h-screen">
-      <div className="flex justify-center items-center min-h-screen">
-          Welcome to Next.js Template
+    <div className='min-h-screen'>
+      <MobileNavbar />
+      <div className='flex justify-center items-center min-h-screen'>
+        Welcome to Next.js Template
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
