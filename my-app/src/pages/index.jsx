af3faa687cc746bdace60fb14ac5cc9f5ee17442
acatarinaoaraujo <acoa_@hotmail.com>
2023-10-30 22:15:43 -1000

    chatbot xc error fix

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,4 +1,4 @@
-//import DialogflowChatWidget from "@/components/chatbot/DialogflowChatWidget";
+import DialogflowChatWidget from "@/components/chatbot/DialogflowChatWidget";
 import MobileNavbar from '@/components/navbar/MobileNavbar';
 const Home = () => {
   return (
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='flex justify-center items-center min-h-screen'>
         Welcome to Next.js Template
       </div>
-      {/* <DialogflowChatWidget/> */}
+      <DialogflowChatWidget/>
     </div>
   );
 };
