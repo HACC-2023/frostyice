d3ebed61ed9cf80e59bed84a2fcee052990de7e7
James Louie Grande <jlgrande@hawaii.edu>
2023-10-31 23:03:41 -1000

    Fix console error

--- my-app/src/components/navbar/MobileNavbar.jsx
+++ my-app/src/components/navbar/MobileNavbar.jsx
@@ -5,31 +5,31 @@ import styles from '../navbar/styles/MobileNavbar.module.css';
 const MobileNavbar = () => {
   return (
     <div className={styles.mobileFooterStyle}>
-      <ul class='flex'>
-        <li class='flex-1 mr-2'>
+      <ul className='flex'>
+        <li className='flex-1 mr-2'>
           <Link
             href='/'
             className='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'>
             HOME
           </Link>
         </li>
-        <li class='flex-1 mr-2'>
+        <li className='flex-1 mr-2'>
           <a
-            class='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
+            className='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
             href='#'>
             REPORT
           </a>
         </li>
-        <li class='flex-1 mr-2'>
+        <li className='flex-1 mr-2'>
           <a
-            class='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
+            className='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
             href='#'>
             DATA
           </a>
         </li>
-        <li class='flex-1 mr-2'>
+        <li className='flex-1 mr-2'>
           <a
-            class='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
+            className='text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4'
             href='#'>
             LOGIN
           </a>
