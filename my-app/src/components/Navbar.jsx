1df815c24d13cb97fb0699ed079d577e5440604f
James Louie Grande <jlgrande@hawaii.edu>
2023-11-01 12:34:04 -1000

    Create sidebar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,328 +1,135 @@
-import { Fragment, useState } from 'react';
-import { useSession } from 'next-auth/react';
-import Link from 'next/link';
-import {
-  Dialog,
-  Disclosure,
-  Popover,
-  Transition,
-  Menu,
-} from '@headlessui/react';
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from '@heroicons/react/20/solid';
+import { Fragment, useState } from "react";
+import { useSession } from "next-auth/react";
+import { HomeIcon, PlusCircleIcon, ChartPieIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
-const products = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding of your traffic',
-    href: '#',
-    icon: ChartPieIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'Security',
-    description: 'Your customers’ data will be safe and secure',
-    href: '#',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Integrations',
-    description: 'Connect with third-party tools',
-    href: '#',
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-];
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '/api/auth/signout' },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
+// TODO: update elements depending on user's role
+// TODO: change icons
+// TODO: add sign out button if logged in
 const Navbar = () => {
   const { data: session } = useSession();
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // have state regarding user's role. this will determine what navbar to show
+
+  const publicNav = [
+    { label: "Home", icon: HomeIcon, link: "/home" },
+    { label: "Report Event", icon: PlusCircleIcon, link: "/report-event" },
+    { label: "Data Insights", icon: ChartPieIcon, link: "/data-insights" },
+  ];
+
+  const orgMemberNav = [
+    ...publicNav, // might change if report and report event and data and data insights are different
+
+    { label: "Organization Dashboard", icon: PlusCircleIcon, link: "/organization-dashboard" },
+    { label: "Manage Events", icon: UserCircleIcon, link: "/manage-events" },
+    { label: "Threads", icon: UserCircleIcon, link: "/threads" },
+  ];
+
+  const orgAdminNav = [
+    ...orgMemberNav,
+    { label: "My Organization", icon: PlusCircleIcon, link: "/my-organization" },
+  ];
+
+  const adminNav = [
+    ...orgAdminNav,
+    { label: "Manage Organizations", icon: PlusCircleIcon, link: "/manage-organizations" },
+  ];
+
+  // check role: "session && session.user.role === "org_admin" && "
+  const NavContainer = () => (
+    <ul className="menu p-5 w-80 min-h-full bg-base-200 pt-14">
+      {!session && <NavContent nav={publicNav} />}
+      {<NavContent nav={orgMemberNav} />}
+      {<NavContent nav={orgAdminNav} />}
+      {<NavContent nav={adminNav} />}
+    </ul>
+  );
+
+  // TODO: change labels
+  const AccountInfo = () => (
+    <div className="flex gap-2 m-2">
+      <UserCircleIcon className="h-12 w-12" />
+      <div className="flex flex-col justify-center">
+        <div>
+          name
+        </div>
+        <div>
+          org name
+        </div>
+      </div>
+
+    </div>
+  );
+
+  // TODO: change naming?
+  const UserInfo = () => <>{!session ? <AccountInfo /> : <MenuItem label={"Login"} link={"/login"} />}</>;
+
+  const NavContent = ({ nav }) => (
+    <>
+      <UserInfo />
+      {nav.map((navItem) => (
+        <MenuItem label={navItem.label} icon={navItem.icon} link={navItem.link} />
+      ))}
+    </>
+  );
+
+  const MenuItem = ({ label, icon, link }) => {
+    const IconElement = icon;
+    const ICON_HEIGHT = 20;
+
+    return (
+      <li className="m-1">
+        <Link href={link}>
+          {icon && <IconElement height={ICON_HEIGHT} />}
+          <span>{label}</span>
+        </Link>
+      </li>
+    );
+  };
 
   return (
-    <header className='bg-white border-b'>
-      <nav
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
-        aria-label='Global'>
-        <div className='flex lg:flex-1'>
-          <Link href='/' className='-m-1.5 p-1.5'>
-            <span className='sr-only'>Your Company</span>
-            <img
-              className='h-8 w-auto'
-              width='100'
-              height='100'
-              src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-              alt=''
-            />
-          </Link>
-        </div>
-        <div className='flex lg:hidden'>
-          <button
-            type='button'
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
-            onClick={() => setMobileMenuOpen(true)}>
-            <span className='sr-only'>Open main menu</span>
-            <Bars3Icon className='h-6 w-6' aria-hidden='true' />
-          </button>
-        </div>
-        <Popover.Group className='hidden lg:flex lg:gap-x-12'>
-          <Popover className='relative'>
-            <Popover.Button className='flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900'>
-              Product
-              <ChevronDownIcon
-                className='h-5 w-5 flex-none text-gray-400'
-                aria-hidden='true'
-              />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter='transition ease-out duration-200'
-              enterFrom='opacity-0 translate-y-1'
-              enterTo='opacity-100 translate-y-0'
-              leave='transition ease-in duration-150'
-              leaveFrom='opacity-100 translate-y-0'
-              leaveTo='opacity-0 translate-y-1'>
-              <Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
-                <div className='p-4'>
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'>
-                      <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-                        <item.icon
-                          className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
-                          aria-hidden='true'
-                        />
-                      </div>
-                      <div className='flex-auto'>
-                        <a
-                          href={item.href}
-                          className='block font-semibold text-gray-900'>
-                          {item.name}
-                          <span className='absolute inset-0' />
-                        </a>
-                        <p className='mt-1 text-gray-600'>{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'>
-                  {callsToAction.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className='flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100'>
-                      <item.icon
-                        className='h-5 w-5 flex-none text-gray-400'
-                        aria-hidden='true'
-                      />
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Features
-          </a>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Marketplace
-          </a>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Company
-          </a>
-          {!session?.user ? (
-            ''
-          ) : (
-            <Link
-              href='/items'
-              className='text-sm font-semibold leading-6 text-gray-900'>
-              Items
-            </Link>
-          )}
-        </Popover.Group>
-        <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
-          {console.log('Session:', session)}
-          {!session?.user ? (
-            <Link
-              href='/auth/credentials-signin'
-              className='text-sm font-semibold leading-6 text-gray-900'>
-              Log in <span aria-hidden='true'>&rarr;</span>
-            </Link>
-          ) : (
-            <Menu as='div' className='relative ml-3'>
-              <div>
-                <Menu.Button className='relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                  <span className='absolute -inset-1.5' />
-                  <span className='sr-only'>Open user menu</span>
-                  <img
-                    className='h-8 w-8 rounded-full'
-                    src='https://placehold.co/400'
-                    alt=''
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter='transition ease-out duration-100'
-                enterFrom='transform opacity-0 scale-95'
-                enterTo='transform opacity-100 scale-100'
-                leave='transition ease-in duration-75'
-                leaveFrom='transform opacity-100 scale-100'
-                leaveTo='transform opacity-0 scale-95'>
-                <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                  {userNavigation.map((item) => (
-                    <Menu.Item key={item.name}>
-                      {({ active }) => (
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            active ? 'bg-gray-100' : '',
-                            'block px-4 py-2 text-sm text-gray-700',
-                          )}>
-                          {item.name}
-                        </a>
-                      )}
-                    </Menu.Item>
-                  ))}
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          )}
-        </div>
-      </nav>
-      <Dialog
-        as='div'
-        className='lg:hidden'
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}>
-        <div className='fixed inset-0 z-10' />
-        <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
-          <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
-              <img
-                className='h-8 w-auto'
-                src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-                alt=''
-              />
-            </a>
-            <button
-              type='button'
-              className='-m-2.5 rounded-md p-2.5 text-gray-700'
-              onClick={() => setMobileMenuOpen(false)}>
-              <span className='sr-only'>Close menu</span>
-              <XMarkIcon className='h-6 w-6' aria-hidden='true' />
-            </button>
-          </div>
-          <div className='mt-6 flow-root'>
-            <div className='-my-6 divide-y divide-gray-500/10'>
-              <div className='space-y-2 py-6'>
-                <Disclosure as='div' className='-mx-3'>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className='flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'rotate-180' : '',
-                            'h-5 w-5 flex-none',
-                          )}
-                          aria-hidden='true'
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className='mt-2 space-y-2'>
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as='a'
-                            href={item.href}
-                            className='block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                  Features
-                </a>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                  Marketplace
-                </a>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                  Company
-                </a>
-              </div>
-              <div className='py-6'>
-                {!session?.user ? (
-                  <a
-                    href='#'
-                    className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                    Log in
-                  </a>
-                ) : (
-                  <div className='mt-3'>
-                    {userNavigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className='block rounded-md py-2 text-base font-medium text-gray-500 hover:bg-gray-50'>
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                )}
-              </div>
+    <>
+      <div className="hidden lg:block w-60">
+        <NavContainer />
+      </div>
+      <div className="drawer hidden md:block lg:hidden">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="w-full navbar">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
             </div>
           </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <NavContainer />
+        </div>
+      </div>
+    </>
   );
 };
 
--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import MobileNavbar from "@/components/navbar/MobileNavbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
@@ -6,8 +7,13 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <>
       <SessionProvider session={session}>
-        <Navbar />
-        <Component {...pageProps} />
+        <div className="flex md:flex-col lg:flex-row">
+          <Navbar/>
+          <div className="flex-grow">
+            <Component {...pageProps} />
+          </div>
+          <MobileNavbar />
+        </div>
       </SessionProvider>
     </>
   );
