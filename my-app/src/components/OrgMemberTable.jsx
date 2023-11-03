3af88edd1d5f0c0320fcd462cb46b6d554541ea1
acatarinaoaraujo <acoa_@hotmail.com>
2023-11-02 22:42:50 -1000

    add modal to delete button

--- my-app/src/components/OrgMemberTable.jsx
+++ my-app/src/components/OrgMemberTable.jsx
@@ -1,4 +1,5 @@
 import { TrashIcon, PencilIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const orgMemberTableDummyData = [
   {
@@ -22,6 +23,22 @@ const orgMemberTableDummyData = [
 ];
 
 const OrgMemberTable = () => {
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  // modal to delete a member
+  const handleDeleteClick = () => {
+    setDeleteModalOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    // add later
+    setDeleteModalOpen(false);
+  };
+
+  const handleCancelDelete = () => {
+    setDeleteModalOpen(false);
+  };
+
   return (
     <div className="overflow-x-auto bg-white">
       <table className="table bg-white text-gray-600">
@@ -58,7 +75,35 @@ const OrgMemberTable = () => {
                 <PencilIcon className="w-4 h-4 text-blue-600 inline-block" />
               </td>
               <td>
-                <TrashIcon className="w-5 h-5 text-red-500" />
+                <TrashIcon
+                  className="w-5 h-5 text-red-500 cursor-pointer"
+                  onClick={handleDeleteClick}
+                />
+                {isDeleteModalOpen && (
+                  <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div
+                      className="fixed inset-0 bg-gray-500 bg-opacity-50"
+                      onClick={handleCancelDelete}
+                    ></div>
+                    <div className="relative p-8 bg-white w-full max-w-lg rounded-lg shadow-lg">
+                      <p>Are you sure you want to delete?</p>
+                      <div className="mt-4 flex justify-end">
+                        <button
+                          className="text-white bg-red-600 hover:bg-red-500 rounded-md px-3 py-2 text-sm font-semibold shadow-sm"
+                          onClick={handleConfirmDelete}
+                        >
+                          Yes
+                        </button>
+                        <button
+                          className="ml-3 text-gray-900 bg-white hover:bg-blue-100 rounded-md px-3 py-2 text-sm font-semibold shadow-sm"
+                          onClick={handleCancelDelete}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </td>
             </tr>
           ))}
