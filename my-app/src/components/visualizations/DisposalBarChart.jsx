de09311151910217347d4aeb4cd44d9ae3504f9e
acatarinaoaraujo <acoa_@hotmail.com>
2023-11-02 23:18:17 -1000

    update chart

--- my-app/src/components/visualizations/DisposalBarChart.jsx
+++ my-app/src/components/visualizations/DisposalBarChart.jsx
@@ -3,17 +3,17 @@ import { useEffect } from "react";
 const DisposalBarChart = () => {
   useEffect(() => {
     import("plotly.js-dist").then((Plotly) => {
-      "";
       const xValue = [
         "Recycled",
         "Research",
         "Storage",
         "Burned",
         "Landfill",
-        "Incineratd",
+        "Incinerated",
       ];
       const yValue = [20, 14, 23, 5, 26, 30];
 
+      // Trace for the bar chart
       const trace1 = {
         x: xValue,
         y: yValue,
@@ -22,21 +22,40 @@ const DisposalBarChart = () => {
         textposition: "auto",
         hoverinfo: "none",
         marker: {
-          color: "rgb(158,202,225)",
+          color: ["rgb(20,120,43)", "rgb(42,140,43)", "rgb(180,160,15)", "rgb(210,100,15)", "rgb(240,80,15)", "rgb(250,39, 39)" ],
           opacity: 0.9,
         },
+        showlegend: false, // Hide the legend for the "Disposal" trace
       };
 
-      const data = [trace1];
+      // Trace for the vertical line
+      const verticalLine = {
+        x: ["Burned", "Burned"],
+        y: [0, Math.max(...yValue)],
+        type: "scatter",
+        mode: "lines",
+        line: {
+          color: "red", // Change the line color to your preference
+          width: 2, // Change the line width to your preference
+        },
+        showlegend: false,
+      };
+
+      const data = [trace1, verticalLine];
 
       const layout = {
+        title: "Good to Worse",
+        titlefont: {
+          size: 13,
+        },
         barmode: "stack",
         margin: { r: 12, t: 20, b: 20, l: 20 },
-        width: 400,
+        width: 380,
         height: 210,
+        showlegend: true, // Show legend for the entire chart
       };
 
-      Plotly.newPlot("disposalBarChart", data, layout);
+      Plotly.newPlot("disposalBarChart", data, layout, {displayModeBar: false});
     });
   }, []);
 
