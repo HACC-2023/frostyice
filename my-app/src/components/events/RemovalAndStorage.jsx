464e401a24802cb34ea90830b0d1602b7fd7ee25
Giorgio <tran.giorgio@gmail.com>
2023-11-04 00:14:37 -1000

    Implement removal step #24

--- my-app/src/components/events/RemovalAndStorage.jsx
+++ my-app/src/components/events/RemovalAndStorage.jsx
@@ -3,7 +3,53 @@ import EventCollapse from "./common/EventCollapse";
 const RemovalAndStorage = ({ event }) => {
   return (
     <EventCollapse title="Removal and Storage">
-      <div> Hello world</div>
+      <div>
+        <section className="flex md:mb-6">
+          <button className="btn btn-secondary justify-self-end">Edit</button>
+        </section>
+        <div className="flex flex-col md:flex-row justify-evenly gap-3 my-3 py-6 px-3 bg-base-100 rounded-xl">
+          <div className="flex flex-col gap-2">
+            <div>
+              <h1 className="md:text-xl font-bold">Debris Size</h1>
+              <p>{event.debrisSize}</p>
+            </div>
+            <div>
+              <h1 className="md:text-xl font-bold">Debris Mass</h1>
+              <p>{event.debrisMass}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>
+              <h1 className="md:text-xl font-bold">
+                Temporary Storage Location
+              </h1>
+              <p>{event.tempStorage}</p>
+            </div>
+            <div>
+              <h1 className="md:text-xl font-bold">Environmental Damage</h1>
+              <p className="break-words">{event.assessedEnvDamage}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>
+              <h1 className="md:text-xl font-bold">Removed By</h1>
+              <p>{event.removedBy}</p>
+            </div>
+            <div>
+              <h1 className="md:text-xl font-bold">Removal Date/s</h1>
+              <p>
+                {event.removalStartDate}-{event.removalEndDate}
+              </p>
+            </div>
+          </div>
+          {/* When edit is clicked, change elements into inputs */}
+          {/* <input className="" value={event.debrisSize} /> */}
+        </div>
+        <section className="flex justify-end gap-3 py-3">
+          {/* <button className="btn btn-outline">Dismiss</button> */}
+          <button className="btn btn-primary">Mark as Removed</button>
+        </section>
+      </div>
     </EventCollapse>
   );
 };
