45a9737b79256361c28919f02a55c2a7710e644f
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-05 14:23:30 -1000

    Update create user route and email

--- my-app/src/pages/api/mongo/user/add-user.js
+++ my-app/src/pages/api/mongo/user/add-user.js
@@ -4,35 +4,44 @@ import bcrypt from 'bcryptjs';
 import { sendEmail } from '@/server/mailService';
 
 export default async function handler(req, res) {
-  try {
-    if (req.method === 'POST') {
-      const { firstName, lastName, email, role, password, orgId } =
-        await req.body;
+  if (req.method === 'POST') {
+    const {firstName, lastName, email, role, password, orgId} = await req.body;
+    try {
+      console.log(`firstName: ${firstName}`)
+      console.log(`lastName: ${lastName}`)
+      console.log(req.body)
       const hashedPassword = await bcrypt.hash(password, 10);
       await connectDB();
       await User.create({
         firstName,
         lastName,
         email,
-        role,
         password: hashedPassword,
-        orgId,
+        orgId: '6541e36d04b6d9f3ddc81ef6', // TODO - remove hard-coded value
+        role: 'org_member', // TODO - remove hard-coded value (?)
       });
+      const emailMessage =
+        `Aloha ${firstName},
+        <br/><br/>
+        Your CMDR account has been created. Account details:
+        <br/>
+        <ul>
+          <li>Name: <b>${lastName}, ${firstName}</b></li>
+          <li>Email: <b>${email}</b></li>
+        </ul>
+        <br/>
+        Mahalo!<br/><br/>
+        <i>Center for Marine Debris Research</i>
+        <br/><br/>
+        <hr/>
+        <i>Please do not reply to this email.</i>`;
+      await sendEmail('Account Created', email, emailMessage);
+      res.status(200).json({msg: 'User added successfully!'});
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Unable to add user to database.' });
     }
-    let emailMessage =
-      'The following user was added to the system.<br/><ul><li> name:' +
-      lastName +
-      ',' +
-      firstName +
-      '</li><li>email:' +
-      email +
-      '</li><li>role:' +
-      role +
-      '</li> <br/><hr/> Thank you and have a nice day.';
-
-    await sendEmail('A New User Added', email, emailMessage);
-    res.status(200).json({ msg: 'User added successfully!' });
-  } catch (error) {
-    res.status(500).json({ error: 'Unable to add user to database.' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
