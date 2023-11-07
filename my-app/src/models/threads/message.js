eed09052cdeacc35cb807862a567133afb367e2e
Giorgio <tran.giorgio@gmail.com>
2023-11-06 16:12:31 -1000

    Create Thread model and endpoints

---
+++ my-app/src/models/threads/message.js
@@ -0,0 +1,27 @@
+const Message = {
+  timestamp: {
+    required: true,
+    type: Date,
+    default: Date.now,
+  },
+
+  authorName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  authorEmail: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+};
+
+export default Message;
