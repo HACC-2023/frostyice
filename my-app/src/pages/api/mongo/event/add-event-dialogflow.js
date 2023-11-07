3c877543085a3e9702c51675193e1c7da2c10f6f
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-07 00:23:30 -1000

    Complete dialogflow event api

--- my-app/src/pages/api/mongo/event/add-event-dialogflow.js
+++ my-app/src/pages/api/mongo/event/add-event-dialogflow.js
@@ -6,60 +6,71 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const {
-        publicType,
-        publicTypeDesc,
-        publicContainerFullness,
-        publicClaimBoat,
-        publicBiofoulingRating,
-        publicLocationDesc,
-        publicLatLongOrPositionDesc,
-        mapLat,
-        mapLong,
-        closestIsland,
-        closestLandmark,
-        debrisLandmarkRelativeLocation,
-        publicDebrisEnvDesc,
-        publicDebrisEnvAdditionalDesc,
-        imageUrl,
-        firstName,
-        lastName,
-        email,
-        phoneNumber,
-      } = await req.body;
+        claim_boat: publicClaimBoat,
+        contact_email: email,
+        contact_phone: phoneNumber,
+        container_filled: publicContainerFullness,
+        debris_additional_desc,
+        debris_coordinates: publicLatLongOrPositionDesc,
+        debris_land_micro_desc,
+        debris_nearest_island: closestIsland,
+        debris_nearby_landmark: closestLandmark,
+        debris_landmark_relative_location: debrisLandmarkRelativeLocation,
+        debris_sea_micro_desc,
+        debris_type: publicType,
+        land_debris_location_desc,
+        marine_life_scale: publicBiofoulingRating,
+        reporter_contact_name,
+        sea_debris_land_distance,
+      } = await req.body.sessionInfo?.parameters;
+
+      const publicLocationDesc = land_debris_location_desc || sea_debris_land_distance;
+      const publicDebrisEnvDesc = debris_land_micro_desc || debris_sea_micro_desc || 'none';
+      const publicDebrisEnvAdditionalDesc = debris_additional_desc;
+
+      // this is sketchy but whatever
+      const firstName = reporter_contact_name[0]?.original?.split(' ')[0];
+      const restOfName = reporter_contact_name[0]?.original?.split(' ');
+      restOfName.shift();
+      const lastName = restOfName.join(' ');
 
       await connectDB();
 
-      const created = await Event.create({
+      const eventDetails = {
         status: 'Reported',
         publicType,
-        publicTypeDesc,
-        publicContainerFullness,
-        publicClaimBoat,
         publicBiofoulingRating,
         publicLocationDesc,
         publicLatLongOrPositionDesc,
-        mapLat,
-        mapLong,
-        closestIsland,
-        closestLandmark,
+        closestLandmark: closestLandmark?.original,
         debrisLandmarkRelativeLocation,
         publicDebrisEnvDesc,
         publicDebrisEnvAdditionalDesc,
-        imageUrl,
         publicContact: {
-          firstName,
-          lastName,
+          firstName: firstName || 'John',
+          lastName: lastName || 'Doe',
           email,
           phoneNumber,
         },
-      });
+      }
+
+      if (publicContainerFullness) {
+        eventDetails.publicContainerFullness = publicContainerFullness;
+      }
+      if (publicClaimBoat) {
+        eventDetails.publicClaimBoat = publicClaimBoat ? 'Yes' : 'No';
+      }
+      if (closestIsland) {
+        eventDetails.closestIsland = closestIsland;
+      }
+
+      const created = await Event.create(eventDetails);
 
       const containerFullness = publicContainerFullness ? `<b>Container Fullness:</b> ${publicContainerFullness}<br/>` : '';
       const claimBoat = publicClaimBoat ? `<b>Intend to Claim Boat:</b> ${publicClaimBoat}<br/>` : '';
       const latLongOrPositionDescription = publicLatLongOrPositionDesc ? `<b>Position Description:</b> ${publicLatLongOrPositionDesc}<br/>` : '';
-      const location = mapLat && mapLong
-        ? `<b>Lat:</b> ${mapLat}<br/><b>Long:</b> ${mapLong}<br/>`
-        : `<b>Nearest Island:</b> ${closestIsland}<br/><b>Nearest Landmark:</b> ${closestLandmark}<br/><b>Landmark Relative Location:</b> ${debrisLandmarkRelativeLocation}<br/>`;
+      const location = publicLocationDesc === 'Greater than three miles from land'
+        ? '' : `<b>Nearest Island:</b> ${closestIsland}<br/><b>Nearest Landmark:</b> ${closestLandmark}<br/><b>Landmark Relative Location:</b> ${debrisLandmarkRelativeLocation}<br/>`;
       const additionalDesc = publicDebrisEnvAdditionalDesc ? `<b>Additional Description:</b> ${publicDebrisEnvAdditionalDesc}<br/>` : '';
 
       // TODO update link at bottom of email
@@ -81,7 +92,7 @@ export default async function handler(req, res) {
         <b>Reporter Name:</b> ${lastName}, ${firstName}<br/>
         <b>Reporter Email:</b> ${email}<br/>
         <b>Reporter Phone:</b> ${phoneNumber}<br/>
-        <b>Report Method:</b> Online Form<br/>
+        <b>Report Method:</b> ${req.body.payload?.telephony ? 'AI Call Center' : 'AI Text Chatbot'}<br/>
         <br/>
         <a href="http://localhost:3000/event/${created._id}">See more details</a>
         <br/><br/>
