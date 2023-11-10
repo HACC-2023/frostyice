1f0085b4bec7cac5f5043f23671631f24d6af826
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-09 22:17:35 -1000

    Add transport endpoints

--- my-app/src/pages/events/all.jsx
+++ my-app/src/pages/events/all.jsx
@@ -2,11 +2,15 @@ import { useState, useEffect } from "react";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import DashboardTable from "@/components/events/events-dashboard/DashboardTable";
 import { useSession } from "next-auth/react";
+import {toast} from "react-toastify";
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
   const [events, setEvents] = useState([]);
   const [storedEvents, setStoredEvents] = useState([]); // events with status 'Removal and Storage'
+  const [selectedEvents, setSelectedEvents] = useState([]); // events selected for multievent shipment
+  const [shipmentDate, setShipmentDate] = useState('');
+  const [fromNode, setFromNode] = useState(''); // org's associatedNode
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +22,13 @@ const Dashboard = () => {
           if (session?.user?.role === "admin") {
             // display all events that have status 'Removal and Storage'
             setStoredEvents(data.filter((event) => event.status === 'Removal and Storage'));
+            setFromNode('CMDR Hub'); // todo change
           } else if (session?.user?.role === "org_admin") {
             // display all events that have status 'Removal and Storage' and tempStorage is the org's associatedNode
             const orgResponse = await fetch(`/api/mongo/organization/id/${session?.user?.orgId}`);
             const orgData = await orgResponse.json();
             setStoredEvents(data.filter((event) => event.status === 'Removal and Storage' && event.tempStorage === orgData.associatedNode));
+            setFromNode(orgData.associatedNode);
           }
         } else {
           console.error("Error fetching data");
@@ -34,6 +40,33 @@ const Dashboard = () => {
 
     fetchData();
   }, [session]);
+
+  async function addToMultiEventTransport() {
+    console.log(shipmentDate, selectedEvents, fromNode)
+    try {
+      const res = await fetch('/api/mongo/event/transport/add', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          shipmentDate: shipmentDate,
+          status: 'Scheduled',
+          eventIds: selectedEvents,
+          fromNode: fromNode,
+        }),
+      });
+      if (res.ok) {
+        toast.success('Added events to shipment');
+        document.getElementById('multieventModal').close();
+      } else {
+        toast.error('Error adding events to shipment');
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error('Error adding events to shipment');
+    }
+  }
 
   return (
     <div className="justify-center items-center">
@@ -60,7 +93,7 @@ const Dashboard = () => {
       <dialog id="multieventModal" className="modal">
         <div class="modal-box bg-white max-w-2xl">
           <h3 className="text-lg font-semibold text-gray-700">
-            Multievent Shipment
+            Shipment to CMDR Hub
           </h3>
           <br />
           <p className="text-gray-600">
@@ -70,6 +103,10 @@ const Dashboard = () => {
           <select
               className="select select-bordered w-full bg-white text-gray-600 p-2"
               multiple
+              style={{ height: '200px' }}
+                onChange={(e) => {
+                  setSelectedEvents([...e.target.selectedOptions].map(o => o.value));
+                }}
           >
             {storedEvents.map((event) => (
                 <option key={event._id} value={event._id}>
@@ -78,28 +115,25 @@ const Dashboard = () => {
             ))}
           </select>
 
-          <div className="flex w-full flex-row mt-8">
+          <div className="flex w-full flex-row">
             <div className="w-2/4">
               <p className="text-gray-600 mt-4 mb-2">
-                <b>Ship From</b>
+                <b>Shipment Date</b>
               </p>
-              <input className="input input-bordered bg-white text-gray-600 mb-2" />
-            </div>
-            <div className="w-2/4">
-              <p className="text-gray-600 mt-4 mb-2">
-                <b>Ship To</b>
-              </p>
-              <input className="input input-bordered bg-white text-gray-600 mb-2" />
+              <input
+                type="date"
+                className="input input-bordered bg-white text-gray-600 mb-2"
+                onChange={(e) => { setShipmentDate(e.target.value) }}
+              />
             </div>
           </div>
           <div className="mt-4 flex justify-end">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
               <button className="text-gray-900 bg-white hover:bg-gray-50 rounded-md px-4 py-2 text-sm font-semibold shadow-sm">Close</button>
             </form>
             <button
                 className="ml-3 text-white bg-blue-600 hover:bg-red-500 rounded-md px-3 py-2 text-sm font-semibold shadow-sm"
-                onClick={() => console.log('submit')}
+                onClick={() => addToMultiEventTransport()}
             >
               Submit
             </button>
