06299d35ce7b0ec1ff5e0ceb5af283381d11556e
James Louie Grande <jlgrande@hawaii.edu>
2023-11-09 17:51:50 -1000

    Work on UI

--- my-app/src/pages/thread/[_id].jsx
+++ my-app/src/pages/thread/[_id].jsx
@@ -3,21 +3,48 @@ import { useSession } from "next-auth/react";
 import { fetcher } from "@/utils/fetcher";
 import useSWR from "swr";
 import Loading from "@/components/Loading";
+import { prettyHstDate } from "@/utils/dateConverter";
 
-const ThreadInfo = ({ thread }) => {
+function getStatusColor(status) {
+  const statusColors = {
+    Reported: "bg-yellow-600",
+    "Removal and Storage": "bg-blue-800",
+    Sorting: "bg-orange-600",
+    Disposal: "bg-purple-700",
+    Complete: "bg-green-700",
+  };
+
+  return statusColors[status] || "bg-gray-500";
+}
+
+const InfoItem = ({ label }) => {
+  return <div className="bg-gray-300 text-gray-800 px-6 py-1 rounded-full text-xs">{label}</div>;
+};
+
+const EventInfo = ({ thread }) => {
   const { data: event, error } = useSWR(`/api/mongo/event/id/${thread.eventId}`, fetcher);
   if (error) return <div>failed to load</div>;
   if (!event) return <Loading />;
 
   return (
-    <div className="bg-base-100 py-3">
-      <div>{event.reportedDate.split("T")[0]}</div>
-      <div>{event.closestIsland}</div>
+    <div className="bg-base-100 py-8 relative shadow-md px-6 rounded-lg">
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">Event Info</h2>
       <div>{event.publicLocationDesc}</div>
-      <div>
+      <span
+        className={`${getStatusColor(
+          event.status
+        )} rounded-full px-2.5 py-0.5 text-sm font-semibold text-white absolute top-4 right-2`}
+      >
+        {event.status}
+      </span>
+      <div className="mb-3">
         {event.publicDebrisEnvDesc === "Other"
           ? event.publicDebrisEnvAdditionalDesc
           : event.publicDebrisEnvDesc}
+      </div>
+      <div className="flex gap-5 flex-wrap">
+        <InfoItem label={prettyHstDate(event.reportedDate)}></InfoItem>
+        <InfoItem label={event.closestIsland}></InfoItem>
       </div>
     </div>
   );
@@ -42,40 +69,52 @@ const ThreadPage = () => {
     // TODO: make a put request to the thread by pushing the message to the messages array
   };
 
-  const Chat = ({ message }) => {
+  const ChatItem = ({ message }) => {
     return (
-      <div>
-        <div className="chat-header">
-          {message.authorName}
-          <time className="text-xs opacity-50">{message.timestamp}</time>
+      <div className="my-4">
+        <div className="flex gap-2 items-center">
+          <div className="font-semibold text-base">{message.authorName}</div>
+          <time className="text-xs opacity-50">{prettyHstDate(message.timestamp)}</time>
         </div>
-        <div>{message.content}</div>
+        <div className="text-gray-800">{message.content}</div>
+      </div>
+    );
+  };
+
+  const MessagesContainer = ({ messages }) => {
+    if (!messages) {
+      return <Loading />;
+    }
+    return (
+      <div className="shadow-md px-6 py-10 rounded-lg">
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">Messages</h2>
+        {messages.length ? (
+          messages.map((message, index) => <ChatItem key={index} message={message}></ChatItem>)
+        ) : (
+          <div className="mb-4">There are currently no messages</div>
+        )}
+        <form onSubmit={handleSumit}>
+          <div className="flex gap-4 w-full">
+            <input
+              type="text"
+              placeholder="Enter your message here..."
+              className="input input-bordered w-full max-w-lg"
+            />
+            <input type="submit" value="Post" className="btn btn-primary max-w-lg" />
+          </div>
+        </form>
       </div>
     );
   };
 
   if (session) {
     return (
-      <div className="w-full min-h-full flex justify-center">
-        <div className="min-h-screen p-5 w-full md:max-w-7xl flex flex-col gap-5">
-          <ThreadInfo thread={thread}></ThreadInfo>
-          <h1 className=" text-xl font-semibold text-gray-800 mb-2">Messages</h1>
-          {thread.messages.length ? (
-            thread.messages.map((message, index) => <Chat key={index} message={message}></Chat>)
-          ) : (
-            <div>There are currently no messages</div>
-          )}
-          <form onSubmit={handleSumit}>
-            <div className="flex gap-4 flex-col md:flex-row w-full">
-              <input
-                type="text"
-                placeholder="Post your message"
-                className="input input-bordered w-full max-w-lg"
-              />
-              <input type="submit" value="Post" className="btn btn-primary max-w-lg" />
-            </div>
-          </form>
-        </div>
+      <div className="m-auto max-w-4xl min-h-screen my-10 px-4">
+        <h1 className="text-4xl font-semibold text-gray-800 mb-10">Thread</h1>
+        <main className="flex flex-col gap-16">
+          <EventInfo thread={thread} />
+          <MessagesContainer messages={thread.messages} />
+        </main>
       </div>
     );
   }
