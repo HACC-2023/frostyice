9bbc8699ea47cf6ddb17f60d6a3f7f2a324fff55
acatarinaoaraujo <acoa_@hotmail.com>
2023-11-10 12:46:06 -1000

    remove unused files

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -65,7 +65,7 @@ export const config = {
     "/admin/:path*",
     "/events/:path*",
     "/home",
-    "/organization",
+    // "/organization",
     "/data-insights",
     "/thread",
     "/threads",
