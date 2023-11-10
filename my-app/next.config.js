667b683e783989161348a6be88db997cfc2e8c3c
Giorgio <tran.giorgio@gmail.com>
2023-11-10 11:47:32 -1000

    Implement image uploading

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/f/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: "/**"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
