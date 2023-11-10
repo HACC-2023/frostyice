17a5a159518f1fbdd293a8f8bfd37bf42660fb25
Giorgio <tran.giorgio@gmail.com>
2023-11-10 13:54:32 -1000

    Fix eslint

--- my-app/src/pages/report/index.jsx
+++ my-app/src/pages/report/index.jsx
@@ -497,25 +497,25 @@ const ReportForm = () => {
           </div>
           {debrisRelativeLocation.includes("sea") ||
           debrisRelativeLocation.includes("None") ? (
-            <span>
-              <p className="text-gray-600 mt-4 mb-4 max-w-2xl">
-                <b>
+              <span>
+                <p className="text-gray-600 mt-4 mb-4 max-w-2xl">
+                  <b>
                   Please provide a position description and any information on
                   currents and winds that could help in relocating the debris.
-                </b>
-              </p>
-              <input
-                type="text"
-                className="input input-bordered bg-white text-gray-600 mb-2 w-full"
-                onChange={(event) =>
-                  setDebrisLocationDetails(event.target.value)
-                }
-                value={debrisLocationDetails}
-              />
-            </span>
-          ) : (
-            ""
-          )}
+                  </b>
+                </p>
+                <input
+                  type="text"
+                  className="input input-bordered bg-white text-gray-600 mb-2 w-full"
+                  onChange={(event) =>
+                    setDebrisLocationDetails(event.target.value)
+                  }
+                  value={debrisLocationDetails}
+                />
+              </span>
+            ) : (
+              ""
+            )}
           <div className="grid flex-grow card rounded-box">
             <div className="mt-4">
               <ClickableMap setCoordinates={setCoordinates} />
