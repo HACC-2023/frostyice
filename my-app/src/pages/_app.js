a18de232fe9c301925b5c0c9ca80c828251decf9
Giorgio <tran.giorgio@gmail.com>
2023-11-11 00:52:52 -1000

    Set text-primary as default font color

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -13,8 +13,10 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <>
       <SessionProvider session={session}>
-        <div className="flex md:flex-col lg:flex-row min-h-screen bg-gradient-to-br from-slate-800 via-cyan-900 to-sky-950
-">
+        <div
+          className="flex md:flex-col lg:flex-row min-h-screen text-primary bg-gradient-to-br from-slate-800 via-cyan-900 to-sky-950
+"
+        >
           {router.pathname !== "/" && <SideNavbar />}
           <div className="flex-1">
             <Component {...pageProps} />
