1c5f2009f14fa8b2d603da082a777d0244f670fd
Shun <saitoshin2145@gmail.com>
2023-11-12 07:38:33 +0900

    Update index.jsx

--- my-app/src/pages/auth/credentials-signin/index.jsx
+++ my-app/src/pages/auth/credentials-signin/index.jsx
@@ -1,9 +1,8 @@
-import { signIn, useSession } from "next-auth/react";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Error from "@/components/Error";
+import { signIn, useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const SignIn = () => {
   const [error, setError] = useState(false);
@@ -12,22 +11,22 @@ const SignIn = () => {
 
   // sends them to the home page
   if (session) {
-    router.push("/");
+    router.push('/');
   }
 
   const { register, handleSubmit } = useForm({
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
       redirect: false,
     },
   });
 
   async function onSubmit(data) {
     console.log(data);
-    const res = await signIn("credentials", data);
+    const res = await signIn('credentials', data);
     if (res.error) {
-      console.log("ERROR:", res.error);
+      console.log('ERROR:', res.error);
       setError(true);
       setTimeout(() => {
         setError(false);
@@ -36,67 +35,64 @@ const SignIn = () => {
   }
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-24 w-auto" src="/logo.png" alt="CMDR" />
-        <h5 className="mt-6 text-secondary text-center text-xl font-semibold">
+    <div className='flex min-h-full flex-1 flex-col justify-center lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        <img className='mx-auto h-24 w-auto' src='/logo.png' alt='CMDR' />
+        <h5 className='mt-6 text-secondary text-center text-xl font-semibold'>
           Center for Marine Debris Research
         </h5>
-        <h6 className="text-center text-secondary text-md font-semibold text-gray-900">
+        <h6 className='text-center text-secondary text-md font-semibold text-gray-900'>
           Marine Debris Reporting Platform
         </h6>
 
-        <div className="shadow-lg mt-8 p-6 rounded-md">
-          <h3 className="text-center text-xl text-primary font-bold leading-9 tracking-tight text-gray-900">
+        <div className='shadow-lg mt-8 p-6 rounded-md'>
+          <h3 className='text-center text-xl text-primary font-bold leading-9 tracking-tight text-gray-900'>
             Are you a member? Sign in here.
           </h3>
 
-          <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className='mt-4 sm:mx-auto sm:w-full sm:max-w-sm'>
             <form
-              className="space-y-6 group"
+              className='space-y-6 group'
               noValidate
-              onSubmit={handleSubmit(onSubmit)}
-            >
+              onSubmit={handleSubmit(onSubmit)}>
               <div>
                 <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                  htmlFor='email'
+                  className='block text-sm font-medium leading-6 text-gray-900'>
                   Email address
                 </label>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    {...register("email")}
-                    id="email"
-                    name="email"
-                    type="email"
+                    {...register('email')}
+                    id='email'
+                    name='email'
+                    type='email'
                     required
-                    placeholder="Example: john@cmdr.com"
-                    className="block w-full bg-white rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder='Example: john@cmdr.com'
+                    className='block w-full bg-white rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
 
               <div>
-                <div className="flex items-center justify-between">
+                <div className='flex items-center justify-between'>
                   <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
+                    htmlFor='password'
+                    className='block text-sm font-medium leading-6 text-gray-900'>
                     Password
                   </label>
                 </div>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    {...register("password")}
-                    id="password"
-                    name="password"
-                    type="password"
+                    {...register('password')}
+                    id='password'
+                    name='password'
+                    type='password'
                     required
-                    placeholder="••••••••••"
-                    className="block w-full bg-white rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500 peer"
+                    placeholder='••••••••••'
+                    className='block w-full bg-white rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500 peer'
                   />
-                  <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                  <span className='mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block'>
                     Please enter a valid password address
                   </span>
                 </div>
@@ -104,9 +100,8 @@ const SignIn = () => {
 
               <div>
                 <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary group-invalid:pointer-events-none group-invalid:opacity-30"
-                >
+                  type='submit'
+                  className='flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary group-invalid:pointer-events-none group-invalid:opacity-30'>
                   Sign in
                 </button>
               </div>
@@ -114,9 +109,8 @@ const SignIn = () => {
 
             <div
               className={`mt-2 py-1.5 px-2 rounded text-sm bg-red-100 text-red-600 border border-red-500 ${
-                error ? "" : "hidden"
-              }`}
-            >
+                error ? '' : 'hidden'
+              }`}>
               Invalid credentials.
             </div>
 
@@ -130,14 +124,13 @@ const SignIn = () => {
               </Link>
             </p> */}
           </div>
-          <div className="divider mt-6 text-secondary font-semibold">OR</div>
-          <div className="grid h-10 mt-4 card rounded-box place-items-center">
-            <p className="text-center text-sm text-gray-500">
-              Make a report as a guest{" "}
+          <div className='divider mt-6 text-secondary font-semibold'>OR</div>
+          <div className='grid h-10 mt-4 card rounded-box place-items-center'>
+            <p className='text-center text-sm text-gray-500'>
+              Make a report as a guest{' '}
               <Link
-                href="/report"
-                className="font-semibold leading-6 text-primary hover:text-secondary"
-              >
+                href='/report'
+                className='font-semibold leading-6 text-primary hover:text-secondary'>
                 here.
               </Link>
             </p>
