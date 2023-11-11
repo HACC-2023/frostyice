8d978ea3ba61bfcb56a3803347eee53128b12038
Giorgio <tran.giorgio@gmail.com>
2023-11-10 16:14:24 -1000

    Fix image uploading bug

--- my-app/src/pages/report/index.jsx
+++ my-app/src/pages/report/index.jsx
@@ -688,12 +688,6 @@ const ReportForm = () => {
                           className="relative cursor-pointer rounded-md bg-white font-semibold focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                         >
                           <span>Upload an Image</span>
-                          <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            className="sr-only"
-                          />
                         </label>
                         <p className="pl-1">or drag and drop</p>
                       </div>
