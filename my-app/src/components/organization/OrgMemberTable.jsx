3767922bddd2346d9695033fb54ee796afcb7237
acatarinaoaraujo <acoa_@hotmail.com>
2023-11-10 23:03:21 -1000

    modify org member checking

--- my-app/src/components/organization/OrgMemberTable.jsx
+++ my-app/src/components/organization/OrgMemberTable.jsx
@@ -13,6 +13,8 @@ const OrgMemberTable = ({ members }) => {
     setEditingRole({ index, role });
   };
 
+  console.log(session?.user.role);
+
   // Update role using API function
   const updateRole = async (userId, newRole) => {
     try {
@@ -77,51 +79,58 @@ const OrgMemberTable = ({ members }) => {
               <th>{member.firstName}</th>
               <th>{member.lastName}</th>
               <td>{member.email}</td>
+
               <td>
-                {editingRole.index === index ? (
-                  <div className="flex-col items-center">
-                    <input
-                      type="text"
-                      className="text-gray-500 bg-white border-2 border-gray-200 rounded-md p-1 w-28 focus:outline-none focus:border-blue"
-                      value={editingRole.role}
-                      onChange={(e) =>
-                        setEditingRole({
-                          index: editingRole.index,
-                          role: e.target.value,
-                        })
-                      }
-                    />
-
-                    <div className="flex-row mt-1">
-                      <button
-                        onClick={() => saveRole(member._id, editingRole.role)}
-                        className="bg-blue-600 text-white hover:bg-blue-500 px-2 py-0.3 rounded"
-                      >
-                        Save
-                      </button>
-
-                      <button
-                        onClick={cancelEdit}
-                        className="bg-red-600 text-white hover:bg-red-500 px-2 py-0.3 rounded ml-2"
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </div>
-                ) : (
-                  // Render role as text with PencilIcon when not editing
-                  <div className="relative">
-                    {member.role}
-                    {editingRole.index !== index && (
-                      <PencilIcon
-                        className="w-4 h-4 text-blue-600 absolute top-0 right-0 cursor-pointer"
-                        onClick={() => toggleEditing(index, member.role)}
+                {session?.user.role !== ROLES.ORG_MEMBER ? (
+                  editingRole.index === index ? (
+                    <div className="flex-col items-center">
+                      <input
+                        type="text"
+                        className="text-gray-500 bg-white border-2 border-gray-200 rounded-md p-1 w-28 focus:outline-none focus:border-blue"
+                        value={editingRole.role}
+                        onChange={(e) =>
+                          setEditingRole({
+                            index: editingRole.index,
+                            role: e.target.value,
+                          })
+                        }
                       />
-                    )}
-                  </div>
+
+                      <div className="flex-row mt-1">
+                        <button
+                          onClick={() => saveRole(member._id, editingRole.role)}
+                          className="bg-blue-600 text-white hover:bg-blue-500 px-2 py-0.3 rounded"
+                        >
+                          Save
+                        </button>
+
+                        <button
+                          onClick={cancelEdit}
+                          className="bg-red-600 text-white hover:bg-red-500 px-2 py-0.3 rounded ml-2"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    // Render role as text with PencilIcon when not editing
+                    <div className="relative">
+                      {member.role}
+                      {editingRole.index !== index && (
+                        <PencilIcon
+                          className="w-4 h-4 text-blue-600 absolute top-0 right-0 cursor-pointer"
+                          onClick={() => toggleEditing(index, member.role)}
+                        />
+                      )}
+                    </div>
+                  )
+                ) : (
+                  // Render only the role as text when the user is an org member
+                  <div>{member.role}</div>
                 )}
               </td>
-              {session?.user.role !== "org_member" && (
+
+              {session?.user.role !== ROLES.ORG_MEMBER && (
                 <td>
                   <button
                     onClick={() =>
