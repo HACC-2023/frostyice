20a948fee3c213bbdb228fb04a9628830d962dac
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-11 02:01:20 -1000

    Update event reported layout

--- my-app/src/components/events/EventReported.jsx
+++ my-app/src/components/events/EventReported.jsx
@@ -12,52 +12,61 @@ const EventRemoval = ({ event, checked, setCurrentChecked }) => {
 
   return (
     <EventCollapse title="Reported" checked={checked} setCurrentChecked={setCurrentChecked} index={0}>
-      <div className="p-3 bg-base-100 rounded-xl">
-        <header>
-          <div className="flex flex-col md:flex-row justify-between">
-            <h3 className="font-bold md:text-xl">Event ID: {event._id}</h3>
-            <time className="font-bold md:text-xl">
-              Date Reported: {prettyHstDate(event.reportedDate)}
-            </time>
-          </div>
-        </header>
-        <section className="py-3">
-          <div className="flex flex-col md:flex-row gap-x-12">
-            <div className="w-full md:w-1/2 h-96 bg-primary-content flex justify-center items-center rounded-md my-3">
-              {event.mapLat && event.mapLong ? (
-                <StaticMap latitude={event.mapLat} longitude={event.mapLong} />
-              ) : (
-                <div className="text-base-100">No coordinates found.</div>
-              )}
+      <div className="bg-base-100 rounded-xl px-6 py-4">
+        <section>
+          <div className="flex flex-col md:flex-row gap-x-6">
+            <div className="w-full md:w-1/2">
+              <h2 className="md:text-2xl font-bold mb-2">Information</h2>
+              <div>
+                <span className="font-semibold">Date Reported:</span> {prettyHstDate(event.reportedDate)}
+                <p className="break-words mt-2"><span className="font-semibold">Type:</span> {event.publicType}</p>
+                {event.publicTypeDesc !== 'No additional description provided' && <p className="break-words">{event.publicTypeDesc}</p>}
+                <p className="break-words mt-2"><span className="font-semibold">Location Description:</span> {event.publicLocationDesc}</p>
+                {event.publicLatLongOrPositionDesc && (<p className="break-words">{event.publicLatLongOrPositionDesc}</p>)}
+                <p className="break-works mt-2"><span className="font-semibold">Environment Description:</span> {event.publicDebrisEnvDesc}</p>
+                {event.publicDebrisEnvAdditionalDesc && (<p className="break-words">{event.publicDebrisEnvAdditionalDesc}</p>)}
+                <p className="break-words mt-2"><span className="font-semibold">Biofouling Rating:</span> {event.publicBiofoulingRating}/10</p>
+                {event.publicContainerFullness && (<p className="break-words">
+                  Container Fullness: {event.publicContainerFullness}
+                </p>)}
+                {event.publicClaimBoat && (<p className="break-words">
+                  Contact Intends to Claim Boat: {event.publicClaimBoat}
+                </p>)}
+              </div>
             </div>
-            <div className="w-full md:w-1/2 flex flex-col gap-x-12">
-              <section className="w-full md:w-3/4">
-                <h2 className="font-bold text-2xl">Location</h2>
+            <div className="w-full md:w-1/2 flex flex-col w-full">
+              <section className="w-full">
+                <h2 className="font-bold md:text-2xl mb-2">Location</h2>
                 <div className="flex justify-between">
-                  <p>Longitude: </p>
-                  <p>{event.mapLong ?? "-"}</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Latitude: </p>
-                  <p>{event.mapLat ?? "-"}</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Island: </p>
+                  <p className="font-semibold">Island: </p>
                   <p>{event.closestIsland ?? "-"}</p>
                 </div>
-              </section>
-              <section className="w-full md:w-3/4">
-                <h2 className="font-bold md:text-2xl">Contact Information</h2>
                 <div className="flex justify-between">
-                  <p>Email: </p>
+                  <p className="font-semibold">Longitude: </p>
+                  <p>{event.mapLong ? Math.round(event.mapLong * 100000) / 100000 : "-"}</p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="font-semibold">Latitude: </p>
+                  <p>{event.mapLat ? Math.round(event.mapLat * 100000) / 100000 : "-"}</p>
+                </div>
+                {event.closestLandmark && (
+                  <p className="break-words">
+                    <span className="font-semibold">Closest Landmark:</span> {event.closestLandmark}: {event.closestLandmark}
+                  </p>
+                )}
+              </section>
+              <section className="w-full">
+                <h2 className="font-bold md:text-2xl mb-2">Contact Information</h2>
+                <div className="flex justify-between">
+                  <p className="font-semibold">Email: </p>
                   <p>{event.publicContact.email}</p>
                 </div>
                 <div className="flex justify-between">
-                  <p>Phone Number: </p>
+                  <p className="font-semibold">Phone Number: </p>
                   <p>{event.publicContact.phoneNumber}</p>
                 </div>
                 <div className="flex justify-between">
-                  <p>Name: </p>
+                  <p className="font-semibold">Name: </p>
                   <p>
                     {event.publicContact.firstName}{" "}
                     {event.publicContact.lastName}
@@ -67,28 +76,16 @@ const EventRemoval = ({ event, checked, setCurrentChecked }) => {
             </div>
           </div>
         </section>
-        <section className="flex flex-col-reverse md:flex-row justify-between w-full gap-x-12 py-3 mb-12">
-          <div className="w-full md:w-1/2">
-            <h2 className="text-2xl font-bold">Information</h2>
-            <ul className="list-disc pl-4 overflow-auto">
-              {event.publicType && (
-                <li className="break-words">Type: {event.publicType}</li>
-              )}
-              {event.publicLocationDesc && <li className="break-words">{event.publicLocationDesc}</li>}
-              {event.publicDebrisEnvDesc && <li className="break-works">{event.publicDebrisEnvDesc}</li>}
-              {event.publicBiofoulingRating && (<li className="break-words">
-                Biofouling Rating - {event.publicBiofoulingRating}
-              </li>)}
-              {event.publicContainerFullness && (<li className="break-words">
-                Container Fullness - {event.publicContainerFullness}
-              </li>)}
-              {event.publicClaimBoat && (<li className="break-words">
-                Contact claims boat - {event.publicClaimBoat}
-              </li>)}
-            </ul>
+        <section className="flex flex-col-reverse md:flex-row justify-between w-full gap-x-6 py-3">
+          <div className="w-full md:w-1/2 h-96 bg-primary-content flex justify-center items-center rounded-md">
+            {event.mapLat && event.mapLong ? (
+              <StaticMap latitude={event.mapLat} longitude={event.mapLong} />
+            ) : (
+              <div className="text-base-100">No coordinates found.</div>
+            )}
           </div>
-          <div className="w-full md:w-1/2 h-96 rounded-md bg-secondary flex items-center justify-center text-base-100 py-3 overflow-hidden">
-            {event.imageUrl ? (<Image src={event.imageUrl} width="800" height="800" alt="event image" className="object-cover"/>) : "No image uploaded."}
+          <div className="w-full md:w-1/2 h-96 rounded-md bg-secondary flex items-center justify-center text-base-100 overflow-hidden">
+            {event.imageUrl ? (<Image src={event.imageUrl} width="800" height="800" alt="event image" className="object-cover w-full h-full"/>) : "No image uploaded."}
           </div>
         </section>
         {/* show this section if the event is not claimed */}
