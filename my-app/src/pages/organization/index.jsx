b80e7dac18329d369ced3bc4ab91f95d922fe8b9
Giorgio <tran.giorgio@gmail.com>
2023-11-11 15:38:09 -1000

    Fix My Organization bug

--- my-app/src/pages/organization/index.jsx
+++ my-app/src/pages/organization/index.jsx
@@ -1,84 +1,114 @@
-import { useState, useEffect } from "react";
+// import { useState } from "react";
 import { useSession } from "next-auth/react";
-import OrgMemberTable from "@/components/organization/OrgMemberTable";
-import AddMemberModal from "@/components/manage-org/modals/AddMemberModal";
 import { ROLES } from "@/roles/roles";
 import Container from "@/components/Container";
-import Error from '@/components/Error';
+import Error from "@/components/Error";
+import useSWR from "swr";
+import { fetcher } from "@/utils/fetcher";
+import AddOrgMemberModal from "@/components/organization/AddOrgMemberModal";
+import UserOrgTableRow from "@/components/organization/UserOrgTableRow";
 
 const MyOrganization = () => {
   const { data: session, status } = useSession();
-  const [serverError, setServerError] = useState(false);
-  const [orgMembers, setOrgMembers] = useState([]);
-  const [organization, setOrganization] = useState([]);
+  // const [serverError, setServerError] = useState(false);
+  // const [orgMembers, setOrgMembers] = useState([]);
+  // const [organization, setOrganization] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (session) {
-          // Fetch user data
-          const userResponse = await fetch('/api/mongo/user/get-users');
-          if (userResponse.ok) {
-            const userData = await userResponse.json();
-            const sameOrgMembers = userData.filter(
-              (user) =>
-                user.orgId === session.user.orgId &&
-                user.email !== session.user.email,
-            );
-            setOrgMembers(sameOrgMembers);
-          } else {
-            setServerError(true);
-          }
+  const { data: orgMembers, error } = useSWR(session ? `/api/mongo/user/get-users-org-id/${session?.user.orgId}` : null, session ? fetcher : null, {
+    refreshInterval: 1000,
+  });
 
-          // Fetch organization data
-          const organizationResponse = await fetch(
-            `/api/mongo/organization/id/${session.user.orgId}`,
-            {
-              method: 'GET',
-            },
-          );
+  console.log("orgmembers", orgMembers);
 
-          if (organizationResponse.ok) {
-            const organizationData = await organizationResponse.json();
-            console.log(organizationData);
-            setOrganization([organizationData]);
-          } else {
-            console.error('Error fetching organization data');
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setServerError(true);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       if (session) {
+  //         // Fetch user data
+  //         const userResponse = await fetch("/api/mongo/user/get-users");
+  //         if (userResponse.ok) {
+  //           const userData = await userResponse.json();
+  //           const sameOrgMembers = userData.filter(
+  //             (user) =>
+  //               user.orgId === session.user.orgId &&
+  //               user.email !== session.user.email
+  //           );
+  //           setOrgMembers(sameOrgMembers);
+  //         } else {
+  //           setServerError(true);
+  //         }
 
-    fetchData();
-  }, [session]);
+  //         // Fetch organization data
+  //         const organizationResponse = await fetch(
+  //           `/api/mongo/organization/id/${session.user.orgId}`,
+  //           {
+  //             method: "GET",
+  //           }
+  //         );
+
+  //         if (organizationResponse.ok) {
+  //           const organizationData = await organizationResponse.json();
+  //           console.log(organizationData);
+  //           setOrganization([organizationData]);
+  //         } else {
+  //           console.error("Error fetching organization data");
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //       setServerError(true);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [session]);
 
   return (
     <Container>
-      {serverError ? <Error /> : null}
-      <div className='mt-2 p-14'>
-        <h3 className='text-2xl font-semibold text-gray-600 mb-2'>
+      {error ? <Error /> : null}
+      <div>
+        <h3 className="text-2xl font-semibold text-primary mb-2">
           My Organization
         </h3>
         <hr />
         <br />
 
-        <div className='p-8 shadow'>
-          {session?.user.role != ROLES.ORG_MEMBER && (
-            <div className='flex flex-row justify-end'>
+        <div>
+          {session?.user.role !== ROLES.ORG_MEMBER && (
+            <div className="flex flex-row justify-end">
               <button
-                className='btn btn-sm md:btn-md btn-primary'
+                className="btn btn-sm md:btn-md btn-primary"
                 onClick={() =>
-                  document.getElementById('add_member_modal_1').showModal()
-                }>
+                  document.getElementById("add_member_modal_1").showModal()
+                }
+              >
                 Add Member
               </button>
-              <AddMemberModal id='add_member_modal_1' orgs={organization} />
+              <AddOrgMemberModal id="add_member_modal_1" orgId={session?.user.orgId} />
             </div>
           )}
-          <OrgMemberTable members={orgMembers} />
+          <div className="w-full mb-12">
+            <h1 className="font-bold px-2 py-3">Members</h1>
+            <div className="h-96 overflow-auto border rounded-md">
+              <div className="overflow-x-auto w-full flex items-center py-3">
+                <table className="table table-zebra">
+                  <thead>
+                    <tr>
+                      <th />
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    { orgMembers && orgMembers.map((user, index) => (
+                      <UserOrgTableRow user={user} index={index} key={index} />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Container>
