d5119c0ebc583c7a2c9fcbebbb274daccc8a723b
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-12 00:05:36 -1000

    Update landing text/buttons

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -6,12 +6,11 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 import { toast } from "react-toastify";
+import bg from "../../public/wave-bg.jpg";
 
 const LocationAggregatorMap = dynamic(() => import("@/components/map/LocationAggregatorMap"), {
   ssr: false,
 });
-
-// TODO: download the image instead of using the url
 
 const Home = () => {
   const [error, setError] = useState(false);
@@ -47,32 +46,37 @@ const Home = () => {
     <div className="min-h-screen">
       {error ? <Error /> : null}
       <div
-        className="relative bg-cover bg-center h-[85vh] border-b border-gray-800"
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?q=80&w=2246&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
-        }}
+        className="relative bg-cover bg-center h-[85vh] border-b border-gray-800 bg-fixed"
+        style={{ backgroundImage: `url(${bg.src})` }}
       >
         <div className="absolute inset-0 gradient-overlay" />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="text-white text-center">
-            <h1 className="text-5xl font-extrabold mb-4">
-              Makai - Marine Debris Reporting Platform
-            </h1>
-            <p className="text-xl text-gray-200">
-              Join the movement to conserve our oceans. See debris? Make a report.
-            </p>
-            <div className="flex gap-2 justify-center mt-10">
-              <Link href="/report">
-                <span className="bg-primary text-white font-bold px-12 py-4 text-lg md:text-base rounded-full">
-                  Make a Report
-                </span>
-              </Link>
-              <Link href="/auth/credentials-signin">
-                <span className="text-white font-bold px-6 py-4 text-lg md:text-base rounded-full border">
-                  Sign in
-                </span>
-              </Link>
+        <div className="text-right fixed top-10 right-3 z-50">
+          <Link href="/auth/credentials-signin">
+            <span className="text-white font-semibold px-6 py-3 md:text-base rounded-xl backdrop-blur hover:bg-sky-400 hover:bg-opacity-30 transition-all">
+              SIGN IN
+            </span>
+          </Link>
+        </div>
+        <div className="fixed inset-0">
+          <div className="flex items-center justify-center align-middle h-full pb-12">
+            <div className="text-white text-center">
+              <h1 className="text-6xl md:text-8xl font-extrabold mb-4">
+                M A K A I
+              </h1>
+              <h1 className="text-xl md:text-4xl font-extrabold mb-4">
+                MARINE DEBRIS REPORTING PLATFORM
+              </h1>
+              <p className="text-lg text-gray-200">
+                <span className="font-semibold">JOIN THE MOVEMENT. </span><span className="font-extrabold">CONSERVE OUR OCEANS.</span>
+              </p>
+              <div className="flex font-semibold gap-2 justify-center mt-10">
+                FOUND DEBRIS?
+                <Link href="/report">
+                  <span className="text-white font-bold px-8 py-3 text-lg md:text-base rounded-xl backdrop-blur bg-sky-600 bg-opacity-30 hover:bg-sky-400 hover:bg-opacity-30 transition-all">
+                    MAKE A REPORT
+                  </span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
