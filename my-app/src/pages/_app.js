597af86c0c3be0f68752f6b8c52d6a17edd9bdc5
James Louie Grande <jlgrande@hawaii.edu>
2023-11-11 16:37:15 -1000

    Show sidebar if signed in only

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,15 +1,11 @@
 import SideNavbar from "@/components/navbar/SideNavbar";
-// import MobileNavbar from "@/components/navbar/MobileNavbar";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import DialogflowChatWidget from "@/components/chatbot/DialogflowChatWidget";
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
-  const router = useRouter();
-
   return (
     <>
       <SessionProvider session={session}>
@@ -17,7 +13,7 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
           className="flex md:flex-col lg:flex-row min-h-screen text-primary bg-gradient-to-br from-slate-800 via-cyan-900 to-sky-950
 "
         >
-          {router.pathname !== "/" && <SideNavbar />}
+          {<SideNavbar />}
           <div className="flex-1">
             <Component {...pageProps} />
           </div>
