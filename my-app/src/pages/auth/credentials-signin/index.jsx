b1b49f1705df3f6dff9f2a5128b02cab3f77e922
Giorgio <tran.giorgio@gmail.com>
2023-11-16 17:51:09 -1000

    Remove email validation

--- my-app/src/pages/auth/credentials-signin/index.jsx
+++ my-app/src/pages/auth/credentials-signin/index.jsx
@@ -70,7 +70,7 @@ const SignIn = () => {
                       {...register("email")}
                       id="email"
                       name="email"
-                      type="email"
+                      // type="email"
                       required
                       placeholder="Example: john@cmdr.com"
                       className="block w-full bg-white rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
