02df2ab067d92f0709ea91bd4dc1665cd10b2e4f
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-16 17:55:46 -1000

    Add title, switch to kg for donut charts

--- my-app/src/pages/_document.js
+++ my-app/src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Makai</title>
+      </Head>
       <body className="min-h-screen">
         <Main />
         <NextScript />
